6e9d2b0eedcf9eb0f76b2f4f553a749f71408eca
Justus-Onyancha <justus.onyancha@moringaschool.com>
2022-03-20 14:30:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+
+// write an equation multiplied to generate number 62
+let num1 = 2;
+let num2 = 31;
+const multiply = num2 * num1;
+
+// let random = Math.floor()
+
+
+//write an equation to generate random numbers greater than  zero
+let random = Math.floor(Math.random() * 100) + 1;
+
+
+// write an equation that calculates and remainder === 4
+let num3 = 104;
+let num4 = 100;
+
+const mod = 104 % 100;
+
+//write an equation that returns 20 as the hightest in the set
+
+let max = Math.max(1,2,3,4,5,6,7,7,8,9,11,20)
+
